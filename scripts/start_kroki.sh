1196316b6c31132980984678136a89a54ba416ae
wonkday <rohit.wakde@gmail.com>
2026-03-26 00:11:44 -0500

    Refactor start_kroki.sh to use environment variables

--- scripts/start_kroki.sh
+++ scripts/start_kroki.sh
@@ -1,1 +1,5 @@
-docker run -p8000:8000 --name kroki-plantuml -d yuzutech/kroki
+#!/bin/bash
+set -a; source <(tr -d '\r' < "$(dirname "$0")/../.env"); set +a
+docker run -p "${KROKI_PORT}:${KROKI_PORT}" \
+  -e PLANTUML_LIMIT_SIZE="${PLANTUML_LIMIT_SIZE:-16384}" \
+  --name "${KROKI_CONTAINER}" -d "${KROKI_IMAGE}"
